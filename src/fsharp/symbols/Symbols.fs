auduchinok/visualfsharp

Add stripped type and qualified base name to FSharpType

src/fsharp/symbols/Symbols.fs
@@ -393,6 +393,18 @@ and FSharpEntity(cenv: SymbolEnv, entity:EntityRef) =
         | CompiledTypeRepr.ILAsmNamed(tref, _, _) -> tref.QualifiedName
         | CompiledTypeRepr.ILAsmOpen _ -> fail()
         
+    member x.QualifiedBaseName = 
+         checkIsResolved()
+         let fail() = invalidOp (sprintf "the type '%s' does not have a qualified name" x.LogicalName)
+ #if !NO_EXTENSIONTYPING
+         if entity.IsTypeAbbrev || entity.IsProvidedErasedTycon || entity.IsNamespace then fail()
+ #else
+         if entity.IsTypeAbbrev || entity.IsNamespace then fail()
+ #endif
+         match entity.CompiledRepresentation with 
+         | CompiledTypeRepr.ILAsmNamed(tref, _, _) -> tref.BasicQualifiedName
+         | CompiledTypeRepr.ILAsmOpen _ -> fail()
+        
     member x.FullName = 
         checkIsResolved()
         match x.TryFullName with 
@@ -2096,6 +2108,17 @@ and FSharpType(cenv, typ:TType) =
     member x.BaseType = 
         GetSuperTypeOfType cenv.g cenv.amap range0 typ
         |> Option.map (fun ty -> FSharpType(cenv, ty)) 
+
+    member x.StrippedType = FSharpType(cenv, stripTyEqnsWrtErasure EraseAll cenv.g typ) 
+    
+    member x.QualifiedBaseName =
+        protect <| fun () -> 
+            match stripTyparEqns typ with 
+            | TType_app (tcref, _) ->
+                match tcref.CompiledRepresentation with 
+                | CompiledTypeRepr.ILAsmNamed(tref, _, _) -> tref.BasicQualifiedName
+                | CompiledTypeRepr.ILAsmOpen _ -> fail() 
+            | _ -> invalidOp "not a stripped type"
 
     member x.Instantiate(instantiation:(FSharpGenericParameter * FSharpType) list) = 
         let typI = instType (instantiation |> List.map (fun (tyv, typ) -> tyv.V, typ.V)) typ

auduchinok/fsharp

src/fsharp/symbols/Symbols.fs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
+﻿// Copyright (c) Microsoft Corporation.  All Rights Reserved.  See License.txt in the project root for license information.
 
 namespace rec FSharp.Compiler.Symbols
 
@@ -431,6 +431,18 @@ type FSharpEntity(cenv: SymbolEnv, entity:EntityRef) =
         match entity.CompiledRepresentation with 
         | CompiledTypeRepr.ILAsmNamed(tref, _, _) -> tref.QualifiedName
         | CompiledTypeRepr.ILAsmOpen _ -> fail()
+        
+    member x.QualifiedBaseName = 
+         checkIsResolved()
+         let fail() = invalidOp (sprintf "the type '%s' does not have a qualified name" x.LogicalName)
+ #if !NO_EXTENSIONTYPING
+         if entity.IsTypeAbbrev || entity.IsProvidedErasedTycon || entity.IsNamespace then fail()
+ #else
+         if entity.IsTypeAbbrev || entity.IsNamespace then fail()
+ #endif
+         match entity.CompiledRepresentation with 
+         | CompiledTypeRepr.ILAsmNamed(tref, _, _) -> tref.BasicQualifiedName
+         | CompiledTypeRepr.ILAsmOpen _ -> fail()
         
     member x.FullName = 
         checkIsResolved()
@@ -2435,6 +2447,19 @@ type FSharpType(cenv, ty:TType) =
     member _.BaseType = 
         GetSuperTypeOfType cenv.g cenv.amap range0 ty
         |> Option.map (fun ty -> FSharpType(cenv, ty)) 
+
+    member x.StrippedType =
+        FSharpType(cenv, stripTyEqnsWrtErasure EraseAll cenv.g ty) 
+
+    member x.QualifiedBaseName =
+        let fail () = invalidOp (sprintf "the type '%O' does not have a qualified name" x)
+        protect <| fun () ->
+            match stripTyparEqns ty with 
+            | TType_app (tcref, _) ->
+                match tcref.CompiledRepresentation with 
+                | CompiledTypeRepr.ILAsmNamed(tref, _, _) -> tref.BasicQualifiedName
+                | CompiledTypeRepr.ILAsmOpen _ -> fail() 
+            | _ -> invalidOp "not a stripped type"
 
     member _.Instantiate(instantiation:(FSharpGenericParameter * FSharpType) list) = 
         let typI = instType (instantiation |> List.map (fun (tyv, ty) -> tyv.V, ty.V)) ty