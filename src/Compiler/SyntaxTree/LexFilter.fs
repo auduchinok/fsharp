48d0c3bcee6c5747272be4a2a1dc7148a993c7e5
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2023-05-22 19:54:15 +0200

    LexFilter: better debugger diagnostics

--- src/Compiler/SyntaxTree/LexFilter.fs
+++ src/Compiler/SyntaxTree/LexFilter.fs
@@ -431,6 +431,8 @@ type LexbufState(startPos: Position,
     member _.EndPos = endPos
     member _.PastEOF = pastEOF
 
+    override this.ToString() = $"({this.StartPos}--{this.EndPos})"
+
 /// Used to save the state related to a token
 /// Treat as though this is read-only.
 [<Class>]
@@ -445,7 +447,9 @@ type TokenTup =
     member x.StartPos = x.LexbufState.StartPos
     /// Returns end position of the token
     member x.EndPos = x.LexbufState.EndPos
-        
+
+    override this.ToString() = $"{this.Token} ({this.StartPos}--{this.EndPos})"
+
 type TokenTupPool() =
 
     /// Arbitrary.
